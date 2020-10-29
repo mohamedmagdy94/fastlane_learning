c39829e28cc4a3f1061a6aecbb6339d0fc748884
Mohamed ElTaweel <mohamedmagdy.9080@gmail.com>
2020-10-29 16:01:34 +0200

    Initial Commit

---
+++ fastlane_learning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fastlane_learning
+//
+//  Created by Mohamed El-Taweel on 10/29/20.
+//  Copyright © 2020 Learning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ fastlane_learningTests/fastlane_learningTests.swift
@@ -0,0 +1,34 @@
+//
+//  fastlane_learningTests.swift
+//  fastlane_learningTests
+//
+//  Created by Mohamed El-Taweel on 10/29/20.
+//  Copyright © 2020 Learning. All rights reserved.
+//
+
+import XCTest
+@testable import fastlane_learning
+
+class fastlane_learningTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
